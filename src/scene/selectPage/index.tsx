7190a0d683eb4c0f4f2d44b117ba9dd995acde64
Park-Mi-Ran <gpfk6636@naver.com>
2021-11-14 21:28:40 +0900

    파일이름 및 classname수정

--- src/scene/selectPage/index.tsx
+++ src/scene/selectPage/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./seletpageScene.module.scss";
+import styles from "./selectScene.module.scss";
 import selectimg from "../../image/main.png";
 import arrow from "../../image/arrow.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,41 +10,41 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import cb from "classnames/bind";
-const sy = cb.bind(styles);
+const cn = cb.bind(styles);
 
 library.add(faStore, faMapMarkedAlt, faAngleDoubleRight);
 
 const SelectScene = () => {
   return (
     <div>
-      <div className={sy("top_content")}>
-        <div className={sy("header")}>
-          <p className={sy("header_text")}>상차림</p>
+      <div className={cn("top_content")}>
+        <div className={cn("header")}>
+          <p className={cn("header_text")}>상차림</p>
         </div>
-        <div className={sy("description")}>
+        <div className={cn("description")}>
           <p>
             차려진 상권에
             <br />
             숟가락만 놓으세요
           </p>
         </div>
-        <div className={sy("img_wrapper")}>
-          <img className={sy("img")} src={selectimg}></img>
+        <div className={cn("img_wrapper")}>
+          <img className={cn("img")} src={selectimg}></img>
         </div>
       </div>
-      <div className={sy("bottom_content")}>
-        <div className={sy("map-card")}>
-          <FontAwesomeIcon icon={faMapMarkedAlt} className={sy("map")} />
-          <p className={sy("map-card-text")}>장소검색</p>
+      <div className={cn("bottom_content")}>
+        <div className={cn("map-card")}>
+          <FontAwesomeIcon icon={faMapMarkedAlt} className={cn("map")} />
+          <p className={cn("map-card-text")}>장소검색</p>
         </div>
-        <div className={sy("store-card")}>
-          <FontAwesomeIcon icon={faStore} className={sy("store")} />
-          <p className={sy("store-card-text")}>업종검색</p>
+        <div className={cn("store-card")}>
+          <FontAwesomeIcon icon={faStore} className={cn("store")} />
+          <p className={cn("store-card-text")}>업종검색</p>
         </div>
       </div>
-      <div className={sy("bottom-card")}>
-        <p className={sy("bottom-card-text")}>상권 둘러보기</p>
-        <img src={arrow} className={sy("arrow")} />
+      <div className={cn("bottom-card")}>
+        <p className={cn("bottom-card-text")}>상권 둘러보기</p>
+        <img src={arrow} className={cn("arrow")} />
       </div>
     </div>
   );
@@ -54,7 +54,3 @@ export default SelectScene;
 
 // 추후할일
 // - 페이지 링크 추가하기
-// - 그림 밑 그림자 px받아서 추가하기
-// 컴펌 받을것
-// - 업종검색 그림자가 너무 튐
-// - 상권 둘러보기 화살표 변경
