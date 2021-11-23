c8500d87a3c1af42d69c2b7540439859b6f07bd4
Park-Mi-Ran <gpfk6636@naver.com>
2021-11-23 16:56:22 +0900

    feat:#8 merging

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,6 @@
 import * as Business from "./business.api";
-<<<<<<< HEAD
 import * as Area from "./area.api";
-
-export { Business, Area };
-=======
 import * as MapAPI from "./map.api";
 import * as CloseMapAPI from "./closeMap.api";
 
-export { Business, MapAPI, CloseMapAPI };
->>>>>>> 5566902e69e05e8095b5910160af176d7ac2adbf
+export { Business, MapAPI, CloseMapAPI, Area };
